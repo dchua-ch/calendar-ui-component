e1d15e5048d02a142733c33f067f64829207bfb1
Daniel <danielchua93@gmail.com>
2021-12-19 16:23:35 +0800

    tried out getNumberOfDaysInMonth

--- date.js
+++ date.js
@@ -12,8 +12,28 @@ console.log(todayNum);
 console.log(todayString);
 console.log(typeof todayString);
 console.log(todayMonth);// i guess months run from 0 to 11
-
-myDate = new Date(1999,0,2);
-console.log(myDate.toDateString());
 console.log(today.getFullYear());
 console.log(typeof today.getFullYear());
+console.log(today.getDay());// apparently 0 is sunday o.o
+console.log(typeof today.getDay());
+console.log("Plying with myDate");
+// seems like months run from 0 to 11, with 0 being january
+myDate = new Date(1999,0,1,0,0,0,0);
+console.log(myDate.toDateString());
+
+console.log(myDate.getDay());
+
+function getNumberOfDaysInMonth(year,month)
+{
+    //day 0 is last day of previous month
+    return new Date(year,month + 1,0).getDate(); 
+}
+
+console.log("Number of days per month in 2021: ")
+for(let i = 0; i < 11; i++)
+{
+    let year = 2021;
+    console.log(getNumberOfDaysInMonth(year,i));
+   
+
+}
