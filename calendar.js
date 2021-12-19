8307a445a9eb4984981d9480d9fbc9b955066294
Daniel <danielchua93@gmail.com>
2021-12-20 04:38:28 +0800

    cleaned up unwanted files

--- calendar.js
+++ calendar.js
@@ -1,66 +1,127 @@
 class Day
 {
+    
     constructor(date)
     {
         //this should be a javascript date object I guess
         this.date = date;
-        this.highlighted = false;
+        this.selected = false;
+        this.year = this.date.getFullYear();
+        this.month = this.date.getMonth();
+        this.day = this.date.getDate();
     }
     
-}
-
-export class Calendar
-{
-
-    constructor()
+    getDateString()
     {
        
+        return `${this.day}/${this.month}/${this.year}`;
+    }
+
+    getHTML()
+    {
+        return  `<div class = \'day\' id = \'${this.getDateString()}\'><p>${this.date.getDate()}</p></div>`;
+    }
+}
+
+class MonthPage
+{
+   
+    days = [];
+    constructor(year,month)
+    {
+        this.year = year;
+        this.month = month;
+        this.generateDays();
+    }
+
+    generateDays()
+    {
+        let firstOfMonth = new Date(this.year,this.month,1,0,0,0);
+     
+        if (firstOfMonth.getDay() != 0)
+        {
+            firstOfMonth.setDate(firstOfMonth.getDate() - firstOfMonth.getDay())
+        }
+        this.days.push(new Day(new Date(firstOfMonth)));
+        for(let i = 0; i < 34; i++)
+        {
+            firstOfMonth.setDate(firstOfMonth.getDate() + 1);
+            this.days.push(new Day(new Date(firstOfMonth)));
+        }
+        // this.days.forEach(day => console.log(day.date));
+
+    }
+
+    getHTML()
+    {
+        let htmlString = '';
+        this.days.forEach(day => {htmlString += day.getHTML()});
+        return htmlString;
+    }
+}
+
+class Calendar
+{
+    //array of MonthPages
+    // each MonthPage are uniquely identified by year and month
+    pages = [];
+    constructor()
+    {
         this.timeElapsed = Date.now();
         this.today = new Date(this.timeElapsed);
-        //this.day = new Day("hehe");
-        //Numbers
-        this.currentMonth = this.today.getMonth();
-        this.currentDay = this.today.getDate();
-        this.currentDayOfWeek = this.today.getDay();
-        this.currentYear = this.today.getFullYear();
-        // this.currentNumberOfDays = getNumberOfDaysInMonth(this.currentMonth,this.currentYear );
-        // this.currentNumberOfDays = this.getNumberOfDaysInMonth();
-        this.updateNumberOfDaysInMonth();
-       
+        this.pageIndicator = new Date(this.today.getFullYear(),this.today.getMonth());
+        this.calendarDiv = document.querySelector(".calendar");
+      
+        this.pages.push(new MonthPage(this.pageIndicator.getFullYear(),this.pageIndicator.getMonth()));
 
 
     }
 
-    updateNumberOfDaysInMonth()
+    createPage()
     {
-        this.currentNumberOfDays = new Date(this.currentYear ,this.currentMonth + 1,0).getDate();
+        // Will only create a new page based on pageIndicator month and year
+        // if page doesn't exist
+        let pageExists = false;
+     
+        for(let i = 0; i < this.pages.length;i++)
+        {
+            if(this.pages[i].month == this.pageIndicator.getMonth() && this.pages[i].year == this.pageIndicator.getFullYear())
+            {
+                pageExists = true;
+            }
+
+        }
+        if( !pageExists)
+        {
+            this.pages.push(new MonthPage(this.pageIndicator.getFullYear(),this.pageIndicator.getMonth()));
+        }
     }
 
     incrementMonth()
     {
-        this.currentMonth += 1;
-        this.updateNumberOfDaysInMonth();
+        this.pageIndicator.setMonth(this.pageIndicator.getMonth() + 1);
+        this.createPage();
         this.render();
     }
 
     decrementMonth()
     {
-        this.currentMonth -= 1;
-        this.updateNumberOfDaysInMonth();
+        this.pageIndicator.setMonth(this.pageIndicator.getMonth() - 1);
+        this.createPage();
         this.render();
     }
 
     render()
     {
         console.log("Rendering calendar");
-        //console.log(this.day.name)
+
         const monthStrings = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December'];
 
-        let calendarDiv = document.querySelector(".calendar");
-        // console.log(calendarDiv);
-
-        // day object ids should be date
+ 
         let renderThis = '';
+
+        // clear html of calendar div
+        this.calendarDiv.innerHTML = renderThis;
 
         // render div with month, year and next/previous buttons
         renderThis += `<h2 class = \'month-year\'>`;
@@ -71,60 +132,62 @@ export class Calendar
         renderThis += '<div>'
 
         // render month 
-        renderThis += `<span class = \'month\'>${monthStrings[this.currentMonth]}</span>`;
+        renderThis += `<span class = \'month\'>${monthStrings[this.pageIndicator.getMonth()]}</span>`;
 
         // render year
-        renderThis += `<span class = \'span\'>${this.currentYear}</span>`;
+        renderThis += `<span class = \'span\'>${this.pageIndicator.getFullYear()}</span>`;
 
         renderThis += '</div>'
 
         // render next button
         renderThis += `<button type = \'button\' class = \'next-previous-button\' onclick = \'nextMonth()\' >\></button></h2>`
-   
-
+        
         // render dayGrid
         renderThis += '<div class = \'day-grid\'>';
 
         // render days of week row
         let dayStrings = ['Sun','Mon','Tue','Wed', 'Thu', 'Fri','Sat'];
         dayStrings.forEach(day => {renderThis += `<div class = \'day-of-week\'>${day}</div>`})
-        console.log(this.currentNumberOfDays);
-        for(let i = 1; i <= 35; i++)
-        {
-            if (i <= this.currentNumberOfDays)
-            {
-                renderThis += `<div class = \'day\'><p>${i}</p></div>`;
-            }
-            else
-            {
-                renderThis += `<div class = \'day\'></div>`;
-            }
 
-        }
+        // render monthPage
+        this.pages.forEach(page => 
+            {
+                if(page.month == this.pageIndicator.getMonth() && page.year == this.pageIndicator.getFullYear())
+                {
+                    let pageHTML = page.getHTML();
+                    // console.log(pageHTML);
+                    renderThis += pageHTML;
+                }
+            }
+            );
 
         renderThis += '</div>';
-        // console.log(renderThis);
-        calendarDiv.innerHTML += renderThis;
+
+        this.calendarDiv.innerHTML = renderThis;
+
     }
-    
 
 }
 
-export function nextMonth()
+
+// const timeElapsed = Date.now();
+// const today = new Date(timeElapsed);
+
+// let monthy = new MonthPage(today.getFullYear(),today.getMonth());
+// monthy.generateDays();
+
+// console.log(monthy.getHTML());
+// console.log(monthy.days[0].getDateString());
+
+let myCalendar = new Calendar();
+myCalendar.render();
+
+
+function nextMonth()
 {
     myCalendar.incrementMonth();
 }
-export function previousMonth()
+function previousMonth()
 {
     myCalendar.decrementMonth();
-}
-
-function getNumberOfDaysInMonth(month,year)
-{
-    //day 0 is last day of previous month
-    // to find number of days in month, get last day of (month + 1) XD
-
-    // e.g. to find number of days in january 2021, create a new date object
-    // corresponding to february 2021 and get last day of previous month
-    return new Date(year,month + 1,0).getDate(); 
 }
