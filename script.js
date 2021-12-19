fb84bb2446756322952916d775c0b7fb70c4148e
Daniel <danielchua93@gmail.com>
2021-12-19 15:59:58 +0800

    removed days of week from html markup and make script.js inject them instead

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let dayGrid = document.querySelector(".day-grid");
-
+let calendarDiv = document.querySelector(".calendar");
 console.log(dayGrid);
 
 
 // day object ids should be date
 let renderThis = '';
+
+let dayStrings = ['Sun','Mon','Tue','Wed', 'Thu', 'Fri','Sat'];
+dayStrings.forEach(day => {renderThis += `<div class = \'day-of-week\'>${day}</div>`})
+
+// render month and year
+
 for(let i = 1; i <= 35; i++)
 {
     if (i <= 31)
